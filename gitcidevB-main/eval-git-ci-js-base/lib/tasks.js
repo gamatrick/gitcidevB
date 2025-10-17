79af48db382975b238c3a4138870a38a6c36e8f4
gamatrick <val@laflyfamily.com>
2025-10-17 14:53:22 +0200

    feat: countDone() add

--- gitcidevB-main/eval-git-ci-js-base/lib/tasks.js
+++ gitcidevB-main/eval-git-ci-js-base/lib/tasks.js
@@ -3,7 +3,6 @@
 let tasks = [];
 let nextId = 1;
 
-
 function getTasks() {
   return tasks;
 }
@@ -11,9 +10,9 @@ function getTasks() {
 function addTask(name) {
   let trimmedName = name.trim();
   let localId = nextId;
-  tasks.push({name: trimmedName, id: localId, done: false});
+  tasks.push({ name: trimmedName, id: localId, done: false });
   nextId++;
-  return {name, id: localId, done: false};
+  return { name, id: localId, done: false };
 }
 
 function reset() {
@@ -24,12 +23,15 @@ function reset() {
 function toggleTask(id) {
   const task = tasks.find(t => t.id === id);
   if (!task) return null;
-
   task.done = !task.done;
   return task;
 }
 
+function countDone() {
+  return tasks.filter(t => t.done).length;
+}
 
-module.exports = { getTasks, reset, toggleTask, addTask };
+module.exports = { getTasks, addTask, reset, toggleTask, countDone };
+
 
 
